df649a5cb6261d2b9fca0699d89ed792efa3c077
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-04 23:28:49 +0700

    declare data class HomeUiState

--- app/src/main/java/com/adrikhamid/administrasisiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/adrikhamid/administrasisiswa/model/HomeViewModel.kt
@@ -1,10 +1,15 @@
 package com.adrikhamid.administrasisiswa.model
 
 import androidx.lifecycle.ViewModel
+import com.adrikhamid.administrasisiswa.data.Siswa
 import com.adrikhamid.administrasisiswa.repositori.RepositoriSiswa
 
 class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel(){
     companion object{
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
