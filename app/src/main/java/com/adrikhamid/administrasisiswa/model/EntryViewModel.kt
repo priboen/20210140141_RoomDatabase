56a193e808a14de2063f5a3178803522a737c3f6
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-04 23:22:26 +0700

    create function for sending value to UiStateSiswa

--- app/src/main/java/com/adrikhamid/administrasisiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/adrikhamid/administrasisiswa/model/EntryViewModel.kt
@@ -15,10 +15,15 @@ data class DetailSiswa(
     val telpon: String = ""
 )
 
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
-    id=id,
-    nama=nama,
-    alamat=alamat,
-    telpon=telpon
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
 
