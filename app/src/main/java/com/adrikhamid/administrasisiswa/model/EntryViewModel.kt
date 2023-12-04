8e05e0db5ef2d4f9b02aa9c5969d4aaa32a495b8
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-04 23:25:54 +0700

    create class EntryViewModel

--- app/src/main/java/com/adrikhamid/administrasisiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/adrikhamid/administrasisiswa/model/EntryViewModel.kt
@@ -1,7 +1,23 @@
 package com.adrikhamid.administrasisiswa.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.adrikhamid.administrasisiswa.data.Siswa
+import com.adrikhamid.administrasisiswa.repositori.RepositoriSiswa
 
+
+class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+}
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
