27f1975083acd1d0a33adb5f9d84148f5e3b5005
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-04 23:16:32 +0700

    create data class DetailSiswa

---
+++ app/src/main/java/com/adrikhamid/administrasisiswa/model/EntryViewModel.kt
@@ -0,0 +1,8 @@
+package com.adrikhamid.administrasisiswa.model
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
