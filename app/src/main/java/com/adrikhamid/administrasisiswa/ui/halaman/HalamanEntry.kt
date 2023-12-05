9b611799c2b93e1d45670a21be15249943b2a263
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-05 17:30:00 +0700

    add Outlinetextfield for nama

--- app/src/main/java/com/adrikhamid/administrasisiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/adrikhamid/administrasisiswa/ui/halaman/HalamanEntry.kt
@@ -2,13 +2,18 @@ package com.adrikhamid.administrasisiswa.ui.halaman
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import com.adrikhamid.administrasisiswa.R
 import com.adrikhamid.administrasisiswa.model.DetailSiswa
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInputSiswa(
     detailSiswa: DetailSiswa,
@@ -20,6 +25,13 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
     ) {
-
+        OutlinedTextField(
+            value = detailSiswa.nama,
+            onValueChange = { onValueChange(detailSiswa.copy(nama = it)) },
+            label = { Text(stringResource(R.string.nama)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
     }
 }
