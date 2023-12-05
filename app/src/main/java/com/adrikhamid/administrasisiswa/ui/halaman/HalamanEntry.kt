d91ee46609876bf3cd2b0045e5df0e826bc542c3
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-05 17:36:31 +0700

    add divider

--- app/src/main/java/com/adrikhamid/administrasisiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/adrikhamid/administrasisiswa/ui/halaman/HalamanEntry.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -59,5 +60,13 @@ fun FormInputSiswa(
                 modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
             )
         }
+        Divider(
+            thickness = dimensionResource(id = R.dimen.padding_small),
+            modifier = Modifier.padding(
+                bottom = dimensionResource(
+                    id = R.dimen.padding_medium
+                )
+            )
+        )
     }
 }
