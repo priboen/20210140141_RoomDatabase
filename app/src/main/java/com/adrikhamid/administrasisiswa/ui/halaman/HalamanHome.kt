dfb2484fc60ef495c9f8f0985f89a35cfdf2d29a
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-05 23:32:03 +0700

    make fun DataSiswa

--- app/src/main/java/com/adrikhamid/administrasisiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/adrikhamid/administrasisiswa/ui/halaman/HalamanHome.kt
@@ -1,9 +1,50 @@
 package com.adrikhamid.administrasisiswa.ui.halaman
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.adrikhamid.administrasisiswa.R
+import com.adrikhamid.administrasisiswa.data.Siswa
 import com.adrikhamid.administrasisiswa.navigasi.DestinasiNavigasi
 
-object DestinasiHome:DestinasiNavigasi{
-    override val route="home"
-    override val titleRes= R.string.app_name
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
+
+@Composable
+fun DataSiswa(
+    siswa: Siswa, modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier, elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = siswa.nama,
+                style = MaterialTheme.typography.titleLarge,
+            )
+            Spacer(Modifier.weight(1f))
+            Icon(
+                imageVector = Icons.Default.Phone,
+                contentDescription = null
+            )
+            Text(
+                text = siswa.alamat,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
 }
