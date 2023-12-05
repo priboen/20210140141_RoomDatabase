c39bb478f4f106540c0b347f3f634be66c7a016f
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-05 23:17:15 +0700

    declare object DestinasiHome in HalamanHome.kt

---
+++ app/src/main/java/com/adrikhamid/administrasisiswa/ui/halaman/HalamanHome.kt
@@ -0,0 +1,9 @@
+package com.adrikhamid.administrasisiswa.ui.halaman
+
+import com.adrikhamid.administrasisiswa.R
+import com.adrikhamid.administrasisiswa.navigasi.DestinasiNavigasi
+
+object DestinasiHome:DestinasiNavigasi{
+    override val route="home"
+    override val titleRes= R.string.app_name
+}
